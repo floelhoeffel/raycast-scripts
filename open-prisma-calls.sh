ba77b6574bc21efeea5c90c33c5eb4d13474ff15
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2022-08-19 13:30:07 +0200

    Add product calls

---
+++ open-prisma-calls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Scratchpad
+# @raycast.packageName Bookmarks
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ✍️
+
+open https://www.notion.so/prismaio/Product-Calls-74a9ecc23e3b40c18b641b38c685b81d
+
+
+
